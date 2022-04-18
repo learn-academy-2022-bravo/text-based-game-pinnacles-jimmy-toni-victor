2ae253f18c4d0b913db7d78182833be4c580b3e1
Velasqj8 <jv_619@yahoo.com>
2022-04-18 11:55:06 -0700

    fixed starting input function

--- Kings-gamble.rb
+++ Kings-gamble.rb
@@ -1,18 +1,31 @@
 
-p 'kings gamble'
-p 'would you like to play a game?'
+p 'Kings Gamble'
+p 'Would you like to play a game?'
 p '(yes)(no)'
-start_game = gets.chomp 
+start_game = gets.chomp
+def turn_1 (turn1)
+    while (!turn1.include? 'no' and !turn1.include? 'yes')
+    p 'do it the right way'
+    turn1 = gets.chomp!
+    end
+        if turn1 == 'no'
+            p 'ok'
+        elsif turn1 == 'yes'
+            p 'lets go'
+        end
+end
 
-
+while (!start_game.include? 'no' and !start_game.include? 'yes')
+p 'do it the right way'
+start_game = gets.chomp!
+end
     if start_game == 'no'
         p 'see ya!'
     elsif start_game == 'yes'
         p 'lets begin'
-    else
-        while (start_game.include? 'no' or start_game.include? 'yes')
-        p 'do it the right way' 
-        start_game = gets.chomp! 
-        end
+        p 'Turn 1'
+        p 'Would you like to move?'
+        p '(yes)(no)'
+        turn1 = gets.chomp
+        turn_1 (turn1)
     end
-
